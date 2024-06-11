6d7e65a001cb99dcc4bcf6319320ff3e3e685eb1
mqqtej <114315264+mqqtej@users.noreply.github.com>
2024-06-11 21:01:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const pizzaFly = document.querySelector('.pizza-fly');
+    const sections = document.querySelectorAll('section');
+
+    pizzaFly.style.animation = 'fly-in-out 4s ease-in-out';
+
+    pizzaFly.addEventListener('animationend', () => {
+        pizzaFly.style.display = 'none';
+        sections.forEach(section => {
+            section.style.opacity = 1;
+        });
+        document.body.style.overflowY = 'auto'; // Re-enable vertical scroll
+    });
+});
