49633aea53cd98f9d23ab6e821113ecbc062ffcd
mqqtej <114315264+mqqtej@users.noreply.github.com>
2024-06-12 14:12:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const pizzaFly = document.querySelector('.pizza-fly');
     const sections = document.querySelectorAll('section');
+    const sideContents = document.querySelectorAll('.side-content');
 
     pizzaFly.style.animation = 'fly-in-out 4s ease-in-out';
+
+    pizzaFly.addEventListener('animationstart', () => {
+        sideContents.forEach(sideContent => {
+            sideContent.style.opacity = 0;
+        });
+    });
 
     pizzaFly.addEventListener('animationend', () => {
         pizzaFly.style.display = 'none';
         sections.forEach(section => {
             section.style.opacity = 1;
+        });
+        sideContents.forEach(sideContent => {
+            sideContent.style.opacity = 1;
         });
         document.body.style.overflowY = 'auto'; // Re-enable vertical scroll
     });
